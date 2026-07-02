ff20c1dffa9b7a35a819efbece7d3f6d38149c25
johnpranoy7 <47387210+johnpranoy7@users.noreply.github.com>
2026-07-02 00:15:54 -0400

    updating port values for portainer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://grpc.johnpranoy7.dev:8080',
+  apiUrl: 'http://grpc.johnpranoy7.dev:8070',
   wsUrl: 'wss://grpc.johnpranoy7.dev/ws/demo/advising'
 };
